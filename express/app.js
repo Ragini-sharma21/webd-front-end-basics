1a4b1faced70ae216577b7dae11b7662e795caef
Ragini sharma <raginisharma6006845094@gmail.com>
2021-11-18 21:20:17 +0530

    express

--- express/app.js
+++ express/app.js
@@ -2,27 +2,19 @@ const express = require("express");  // we have imported the module
 const path = require("path");  // path module
 const app = express(); // we have made the express app
 const port = 80;
-// for serving static files 
+// EXPRESS SPECIFIC STUFF
 app.use('/static',express.static('static')) //'static' is the folder name
 
-// set the template engine as pug
+//PUG SPECIFIC STUFF
 app.set('view engine', 'pug');
-
-//set the views diectory
 app.set('views',path.join(__dirname,'views'))  //dirname=directory name
-
-//our pug demo endpoint
-app.get("/demo" , (req,res)=>{        // whenever we will encounter this /demo or enter this folder ..this arrow function will give " this ....." response
-    res.status(200).render('demo',{title: 'hey ragini' , message:'how are you '})
+//ENDPOINTS
+app.get('/',(req,res)=>{
+    const con="this is the best content";
+    const params = {'title': 'pubg is the best game ', 'content':con}
+    res.status(200).render('index.pug',params);
 })
-
-app.get("/" , (req,res)=>{        // whenever we will encounter this / or enter this folder ..this arrow function will give " this ....." response
-    res.send("This is my first home app ")
-})
-
-app.get("/about" , (req,res)=>{        // whenever we will encounter this /about or enter this folder ..this arrow function will give " this ....." response
-    res.send("This is my first about app ")
-})
+//START THE SERVER
 app.listen(port,()=>{
     console.log(`the application started successfully ${port}`);
 
